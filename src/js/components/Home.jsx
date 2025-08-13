b51be5990dc555525d0e74b1f96f8ad16596e4e1
Mili83666 <milagrocaba@gmail.com>
2025-08-13 15:44:54 +0000

    Firts Commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	//let nuevoTodo= "";
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [nuevoTodo, setnuevoTodo] = useState("");
+	const [todos, setTodos] = useState([]);
+
+	const handleClick = () => {
+		console.log("hola")
+		setTodos([...todos, nuevoTodo])
+	}
+
+	const deleteTodo = (indice) => {
+		const listaNueva = todos.filter((todo,i) => i !== indice)
+		setTodos(listaNueva);
+
+	}
+
+	const handleChange = (event) => {
+		setnuevoTodo (event.target.value)
+
+	}
+
+
+
+	return (
+		<div className="container">
+			<div>
+				<h1>To do list</h1>
+			</div>
+			<div>
+
+		    <input type= "text" onChange={handleChange}></input>
+
+			<button onClick={handleClick}>
+				Agregar tarea
+			</button>
+
+			</div>
+			<div>
+				<p>Nueva tarea : {nuevoTodo}</p>
+			</div>
+			<ul>
+				{todos.map((todo, indice) => {
+					return (
+						<li>
+							{todo}<button onClick={()=> deleteTodo (indice)}>Borrar</button>
+
+						</li>
+					)
+				})}
+			</ul>
+
+
+          
 		</div>
 	);
 };
